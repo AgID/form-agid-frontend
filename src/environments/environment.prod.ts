bd4f5943f4d39708255986f27e5b6697d49b861b
Vincenzo Aimone <v.aimone@almaviva.it>
2022-12-12 16:20:12 +0100

    other: rimosso lo slash dal redirect uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   AUTH_CLIENT_ID: '4af1e6aa-15e2-4e22-98b0-cf6520377b52',
   AUTH_DUMMY_CLIENT_SECRET: '78bc23ef-f431-44f3-8544-c59fa0b32634',
   AUTH_BASIC_AUTH: true,
-  AUTH_REDIRECT_URI: 'https://va-form.agid.gov.it/',
+  AUTH_REDIRECT_URI: 'https://va-form.agid.gov.it',
   AUTH_SCOPE: 'openid profile email offline_access',
   AUTH_RESPONSE_TYPE: 'code',
   AUTH_SHOW_DEBUG_INFORMATION: true,
