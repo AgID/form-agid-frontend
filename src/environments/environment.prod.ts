6f9fbb9e24609e1aa4ee38823ce002c4ff8b1a9d
federico <federico.serafini@digitaliasistemi.it>
2023-12-20 17:19:41 +0100

    aggiornamento auth_issuer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   BACKEND_HOST: '/api',
   BACKEND_AMM: '/api',
-  AUTH_ISSUER: 'https://login-test.agid.gov.it',
+  AUTH_ISSUER: 'https://login.agid.gov.it',
   AUTH_CLIENT_ID: '4af1e6aa-15e2-4e22-98b0-cf6520377b52',
   AUTH_DUMMY_CLIENT_SECRET: '78bc23ef-f431-44f3-8544-c59fa0b32634',
   AUTH_BASIC_AUTH: true,
